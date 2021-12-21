bf5ceebabf452bd34c368878704f5be428f03984
Therese Grass <tg222kv@student.lnu.se>
2021-12-21 09:47:21 +0100

    adding buttons to chat-app

--- src/js/components/chat-app/chat-app.js
+++ src/js/components/chat-app/chat-app.js
@@ -26,11 +26,11 @@ template.innerHTML = `
 .chatinput {
     background: black;
     position: absolute;
-    margin-bottom: 13px;
+    margin-bottom: 12px;
     margin-left: 5px;
     bottom: 0px;
     background: white;
-    max-width: 360px;
+    max-width: 310px;
     height: 38px;
     border: 1px solid #222222;
 }
@@ -43,9 +43,9 @@ template.innerHTML = `
     background: #222222;    
 }
 
-  button {
+  .paperplane {
     right: 0;
-    margin-bottom: 13px;
+    margin-bottom: 12px;
     margin-right: 5px;
     position: absolute;
     bottom: 0px;
@@ -56,16 +56,51 @@ template.innerHTML = `
     background-size: cover;
     cursor: pointer;      
 }
+    .giphy {
+        right: 0;
+    margin-bottom: 19px;
+    margin-right: 45px;
+    position: absolute;
+    bottom: 0px;
+    width: 30px;
+    height: 30px;
+    border: none;
+    background-image: url('js/components/chat-app/lib/giphylogo.svg');
+    background-size: 25px 25px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-color: transparent;
+    overflow: visible;
+    }
+
+    .giphysearchwindow {
+        right: 0;
+        width: 250px;
+    height: 250px;
+    border: 2px solid red;
+    z-index: 50;
+    }
+
+    .gifsearchfield {
+        padding-left: 2px;
+        padding-right: 20px;
+    }
+
+
+
+
 </style>
 
   <div class="chatwrapper">
       <div class="inputbackground">
          <form>
           <textarea class="chatinput" name="message" rows="4" cols="50"></textarea>
-          <button></button>
+          <button class="giphy"></button>
+          <button class="paperplane"></button>
           </form>
       </div>
     <div class="chatwindow">
+    <div class="giphysearchwindow"><input class="gifsearchfield" type="text" value="" placeholder="What gif are you looking for?"></div>
 </div>
   </div>
 `
@@ -82,7 +117,44 @@ customElements.define('chat-app',
 
       this.attachShadow({ mode: 'open' })
         .appendChild(template.content.cloneNode(true))
-        this.sendMessageButton = this.shadowRoot.querySelector('button')
+        this.sendMessageButton = this.shadowRoot.querySelector('.paperplane')
+        this.showGifWindowButton = this.shadowRoot.querySelector('.giphy')
+        this.gifSearchField = this.shadowRoot.querySelector('.gifsearchfield')
+        this.chatInputField = this.shadowRoot.querySelector('.chatinput')
 
+      this.sendMessageButton.addEventListener('click', (event) => {
+        event.preventDefault()
+        console.log('knapp')
+      })
+
+      this.showGifWindowButton = this.shadowRoot.querySelector('.giphy')
+        .addEventListener('click', (event) => {
+          event.preventDefault()
+          console.log('knapp')
+        })
+
+      this.gifSearchField.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          event.preventDefault()
+          console.log('gifsearchField')
+        }
+      })
+
+      this.chatInputField.addEventListener('keypress', (event) => {
+        if (event.key === 'Enter') {
+          event.preventDefault()
+          console.log('chatinputfield')
+        }
+      })
     }
+
+    async getImages() {
+        const url = 'api.giphy.com/v1/gifs/search?q=' + this.input.value + '&rating=g&api_key=yQC3qMgBqPWBQDkjDQo7KkJqvY1GiFoH'
+        const fetchedUrl = await fetch(url, {
+          headers: {
+            Authorization: 'Client-ID iczfs_vxPOCtlCw-rifNqkkcu5hxrangGv7GVR3br3s'
+          }
+        })
+        const response = await fetchedUrl.json()
+      }
   })
